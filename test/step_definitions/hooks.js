4f2169ea23c06acae9f92f707e0795f6d1469f5a
Yury Hamaniuk <yury_hamaniuk@epam.com>
2020-06-18 14:20:02 +0300

    clean directories

--- test/step_definitions/hooks.js
+++ test/step_definitions/hooks.js
@@ -1,6 +1,6 @@
 const { After } = require('cucumber');
 
-After({tags: '@so'}, async function(){
+After({tags: '@epam'}, async function(){
     const screenshot = await browser.takeScreenshot();
     const decodedImage = new Buffer.from(screenshot, 'base64');
     return this.attach(decodedImage, 'image/png');
