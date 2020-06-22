b15bf5d1c6f677f34a55604583c26394040d2ced
Yury Hamaniuk <yury_hamaniuk@epam.com>
2020-06-22 18:18:37 +0300

    fix

--- test/utils/page_object/what_we_do_page/what_we_do_page.js
+++ test/utils/page_object/what_we_do_page/what_we_do_page.js
@@ -4,11 +4,7 @@ const Element = require('../base_elements/base_element');
 class WhatWeDoPage extends BasePage {  
   constructor() {
     super();
-    this.title = () => {
-      let element = new Element('h1.title-ui');
-      super.waitForVisibility(element, 5000);      
-      return element;
-    }
+    this.title = new Element('h1.title-ui');      
   }
 }
 
